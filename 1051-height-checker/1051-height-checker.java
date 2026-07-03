d73af6d2eb7150589cd569eaf1160d0102cac89f
Nitu Chaudhary <nituchaudhary467@gmail.com>
2026-07-03 21:44:41 +0530

    Time: 0 ms (100%), Space: 43.1 MB (85.98%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,11 +1,17 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int[] expected = heights.clone();
-        Arrays.sort(expected);
+        int[] freq = new int[101];
+        for(int h:heights) freq[h]++;
         int count =0;
+        int currentheight=1;
         for(int i=0;i<heights.length;i++){
-            if(heights[i] != expected[i])
-            count++;
+            while(freq[currentheight]==0){
+                currentheight++;
+            }
+            if(heights[i] != currentheight){
+                count++;
+            }
+            freq[currentheight]--;
         }
         return count;
     }
