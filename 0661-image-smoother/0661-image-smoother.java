a8484ef3c1d5f0120062c2fc4712c03ded434047
Nitu Chaudhary <nituchaudhary467@gmail.com>
2026-07-06 22:28:00 +0530

    Time: 7 ms (34.73%), Space: 48.2 MB (5.6%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.java
+++ 0661-image-smoother/0661-image-smoother.java
@@ -1,22 +1,23 @@
 class Solution {
     public int[][] imageSmoother(int[][] img) {
-        int row =img.length;
-        int col =img[0].length;
-        int[][] ans = new int[row][col];
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
+       int m =img.length;
+        int n =img[0].length;
+        int[] dr ={-1,-1,-1,0,0,0,1,1,1};
+        int[] dc={-1,1,0,-1,1,0,-1,1,0};
+        int[][] ans =new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 int sum=0;
-                int count =0;
-                for(int r = i-1;r<=i+1;r++ ){
-                    for(int c=j-1;c<=j+1;c++){
-                        if(r>=0 && r<row && c>=0 && c<col){
-                        sum += img[r][c];
+                int count=0;
+                for(int k=0;k<9;k++){
+                    int nr = i+dr[k];
+                    int nc =j+dc[k];
+                    if(nr>=0 && nr <m &&nc>=0 && nc<n){
+                        sum += img[nr][nc];
                         count++;
-                        }
                     }
                 }
-            
-            ans[i][j] = sum/count;
+                ans[i][j] =sum/count;
             }
         }
         return ans;
