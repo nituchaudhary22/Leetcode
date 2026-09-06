af48d68665ef1563a5303fd93ca372e1a5e1d2eb
Nitu Chaudhary <nituchaudhary467@gmail.com>
2026-09-06 22:17:09 +0530

    Time: 0 ms (100%), Space: 53.1 MB (21.85%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,10 +1,21 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int ans =0;
-        for(int num : nums){
-            ans = ans ^ num;
+        int low = 0;
+        int high = nums.length - 1;
+        
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            
+            // Check if mid is correctly paired with its neighbor
+            if (nums[mid] == nums[mid ^ 1]) {
+                // The single element is further to the right
+                low = mid + 1;
+            } else {
+                // The single element is at mid or to the left
+                high = mid;
+            }
         }
-
-        return ans;
+        
+        return nums[low];
     }
 }
